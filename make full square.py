5800db931d82bed5c9915f8481d144d1f0b777d2
mehmetemrekayacan <emremehmet32@hotmail.com>
2023-02-13 22:16:44 +0300

    tm

---
+++ make full square.py
@@ -0,0 +1,12 @@
+x=int(input("seklin enini gir: "))
+y=int(input("şeklin boyunu gir: "))
+liste=[]
+def abc():
+    for i in range(1,x+1):
+        i="*"
+        print("\n",end="")
+        print(i,end=" ")
+        for n in range(1,y):
+            n="*"
+            print(n,end=" ")
+liste.append(abc())
